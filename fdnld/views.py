bd7bbb55b44166b81a348997a885a8c558a542f8
Will Norvelle <willnorvelle@gmail.com>
2017-10-29 18:10:00 -0400

    allow passing of file name to download

--- fdnld/views.py
+++ fdnld/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def test_text(request):
     with open('./media/test_text.txt', 'rb') as file:
         response = HttpResponse(file.read())
         response['Content-Type'] = 'text'
         response['Content-Disposition'] = 'attachment; filename="test_text.txt"'
+    return response
+
+
+def download_file(request, filename):
+    with open('./media/' + filename, 'rb') as file:
+        response = HttpResponse(file.read())
+        response['Content-Type'] = 'application/octet-stream'
+        response['Content-Disposition'] = 'attachment; filename="' + filename + '"'
     return response
